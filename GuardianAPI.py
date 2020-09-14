abc3106fbd4621a9ff2d87b192ee4ab17d7cc015
Mathias Haugestad <mhaugestad@gmail.com>
2020-09-14 20:21:55 +0100

    update GuardianAPI

--- GuardianAPI.py
+++ GuardianAPI.py
@@ -3,37 +3,40 @@
 
 ## The Guardian ""
 import requests
+from requests_toolbelt import sessions
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import json
 import getpass
 
-class Guardian_API:
+class GuardianAPI:
     
     def __init__(self):
         self.api_key = getpass.getpass('api_key: ')
+        self.endpoint = sessions.BaseUrlSession(base_url="https://content.guardianapis.com")
+        adapter = HTTPAdapter(max_retries=Retry(total = 3,
+                                                backoff_factor = 1,
+                                                status_forcelist = [429, 500, 502, 503, 504]))
+        self.endpoint.mount('https://', adapter)
     
     def get_content(self, parameters):
-                
-        parameter_string = ""
-        for next in parameters.items():
-            parameter_string +="{}={}&".format(next[0],next[1])
+        response = self.endpoint.get("/search", params = parameters, headers = {'api-key':self.api_key,
+                                                                                'format':'json'})
+        if response.status_code != 200:
+            raise Exception('API Error; status_code:{}, status:{}'.format(response.status_code, response.text))
         
-        #print(parameter_string)
-        response = requests.get("https://content.guardianapis.com/search?{}page=1&api-key={}".format(parameter_string, self.api_key))
-        response_json = json.loads(response.text)
-                
-        if response_json["response"]["status"] != "ok":
-            print(response_json["response"])
-            return([])
-        
+
+        response_json = json.loads(response.text)                
         all_results = []
         all_results.extend(response_json["response"]["results"])
-        
         npages = response_json["response"]["pages"]
         for page in range(2,npages+1):
-            page_response = requests.get("https://content.guardianapis.com/search?{}page={}&api-key={}".format(parameter_string, page, self.api_key))
-            
+            parameters['page'] = page
+            page_response = self.endpoint.get("/search", params = parameters, headers = {'api-key':self.api_key,
+                                                                                'format':'json'})
+            if page_response.status_code != 200:
+                raise Exception('API Error; status_code:{}, status:{}'.format(response.status_code, response.text))
+    
             page_response_json = json.loads(page_response.text)
-
             all_results.extend(page_response_json["response"]["results"])
-        
         return all_results
